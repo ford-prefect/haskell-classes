afa957dde0d2926550de07ee8e7e42a8b874f969
Arun Raghavan <arun@arunraghavan.net>
2018-01-19 15:38:57 +0530

    Implement a few more Monoid instances

--- year3/monoid.hs
+++ year3/monoid.hs
@@ -19,3 +19,74 @@ instance Monoid a => Monoid (Mebbe a) where
   mappend x Nada            = x
   mappend Nada x            = x
   mappend (Vada x) (Vada y) = Vada (x <> y)
+
+
+newtype First a = First (Maybe a) deriving (Show)
+
+instance Monoid (First a) where
+  mempty = First Nothing
+  mappend x@(First (Just _)) _ = x
+  mappend (First Nothing) x    = x
+
+newtype Last a = Last (Maybe a) deriving (Show)
+
+instance Monoid (Last a) where
+  mempty = Last Nothing
+  mappend _ x@(Last (Just _)) = x
+  mappend x (Last Nothing)    = x
+
+
+newtype All = All Bool deriving (Show)
+
+instance Monoid All where
+  mempty = All True
+  mappend (All False) _         = All False
+  mappend _ (All False)         = All False
+  mappend (All True) (All True) = All True
+
+newtype Any = Any Bool deriving (Show)
+
+instance Monoid Any where
+  mempty = Any False
+  mappend (Any True) _            = Any True
+  mappend _ (Any True)            = Any True
+  mappend (Any False) (Any False) = Any False
+
+
+newtype Min a = Min a deriving (Show)
+
+instance (Ord a, Bounded a) => Monoid (Min a) where
+  mempty = Min maxBound
+  mappend (Min x) (Min y) = Min $ min x y
+
+newtype Max a = Max a deriving (Show)
+
+instance (Ord a, Bounded a) => Monoid (Max a) where
+  mempty = Max maxBound
+  mappend (Max x) (Max y) = Max $ max x y
+
+
+data List a = Cons a (List a) | Empty deriving (Show)
+
+instance Monoid (List a) where
+  mempty = Empty
+  mappend Empty b       = b
+  mappend a Empty       = a
+  mappend (Cons a as) b = Cons a (as `mappend` b)
+
+
+data BST a = Node a (BST a) (BST a) | EmptyNode deriving (Show)
+
+insert :: Ord a => BST a -> a -> BST a
+insert EmptyNode x = Node x EmptyNode EmptyNode
+insert (Node v l r) x
+  | x < v     = Node v (insert l x) r
+  | x > v     = Node v l (insert r x)
+  | otherwise = error "Tried to insert duplicate"
+
+instance Ord a => Monoid (BST a) where
+  mempty = EmptyNode
+
+  mappend EmptyNode b    = b
+  mappend a EmptyNode    = a
+  mappend (Node v l r) b = l `mappend` insert b v `mappend` r
