51ec0fd9726a90a97dce6fb3859f5b9bc75c7ae8
Arun Raghavan <arun@arunraghavan.net>
2018-01-27 11:03:14 +0530

    sudoku: Fill in main to actually run the code

--- MiDokuSuDoku/app/Main.hs
+++ MiDokuSuDoku/app/Main.hs
@@ -1,6 +1,14 @@
 module Main where
 
+import System.Environment (getArgs)
+
 import Sudoku
 
 main :: IO ()
-main = undefined
+main = do
+  [s] <- getArgs
+  case readBoard s of
+    Nothing -> putStrLn "Invalid board"
+    Just b  -> case solve b of
+      Nothing -> putStrLn "No solution found"
+      Just b' -> putStrLn $ prettyBoard b'
