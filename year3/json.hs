fef258c34ed7dbbd0b74f6ccdad1129ff22f5f82
Arun Raghavan <arun@arunraghavan.net>
2018-02-15 20:04:45 +0530

    Complete the JSON parser

--- year3/json.hs
+++ year3/json.hs
@@ -1,21 +1,28 @@
 module Json where
 
-newtype JsonString = JsonStr String deriving (Show)
+import Control.Applicative
+import Control.Monad (replicateM)
+import Data.Char (chr, digitToInt, isDigit, isHexDigit, isSpace)
+import Data.Functor
 
-data JsonNumber = JsonInt Integer
-                | JsonIntFrac Integer Integer
-                | JsonIntExp Integer Integer
-                | JsonIntFracExp Integer Integer Integer
+newtype JsonStr = JsonStr String
 
-instance Show JsonNumber where
-  show (JsonInt i)            = show i
-  show (JsonIntFrac i f)      = show i ++ "." ++ show f
-  show (JsonIntExp i e)       = show i ++ "e" ++ show e
-  show (JsonIntFracExp i f e) = show i ++ "." ++ show f ++ "e" ++ show e
+instance Show JsonStr where
+  show (JsonStr s) = show s
 
-data JsonValue = JsonString JsonString
-               | JsonNumber JsonNumber
-               | JsonObject [(JsonString, JsonValue)]
+data JsonNum = JsonNum { signif :: Integer
+                       , expo   :: Integer
+                       }
+
+instance Show JsonNum where
+  show num@(JsonNum n e) = show n ++ "e" ++ show e ++ " (" ++ show (toDouble num) ++ ")"
+
+toDouble :: JsonNum -> Double
+toDouble (JsonNum n e) = fromIntegral n * 10 ^^ e
+
+data JsonValue = JsonString JsonStr
+               | JsonNumber JsonNum
+               | JsonObject [(JsonStr, JsonValue)]
                | JsonArray [JsonValue]
                | JsonBool Bool
                | JsonNull
@@ -35,3 +42,127 @@ instance Show JsonValue where
       showElements _ []     = ""
       showElements s [v]    = s v
       showElements s (v:vs) = s v ++ ", " ++ showElements s vs
+
+
+newtype Parser i o = Parser { runParser :: i -> (Maybe o, i) }
+
+instance Functor (Parser i) where
+  fmap f p = Parser $ \input ->
+    let (mo, i) = runParser p input
+    in (f <$> mo, i)
+
+instance Applicative (Parser i) where
+  pure x = Parser $ \input -> (Just x, input)
+
+  pf <*> po = Parser $ \input ->
+    case runParser pf input of
+         (Just f, rest) -> case runParser po rest of
+                                (Just o, rest') -> (Just (f o), rest')
+                                (Nothing, _)    -> (Nothing, input)
+         (Nothing, _)   -> (Nothing, input)
+
+instance Alternative (Parser i) where
+  empty = Parser $ \input -> (Nothing, input)
+  p1 <|> p2 = Parser $ \input ->
+      case runParser p1 input of
+           (Nothing, _) -> case runParser p2 input of
+                                (Nothing, _) -> (Nothing, input)
+                                justValue    -> justValue
+           justValue    -> justValue
+
+satisfy :: (Char -> Bool) -> Parser String Char
+satisfy p = Parser $ \input ->
+  case input of
+       (c:cs) | p c -> (Just c, cs)
+       _            -> (Nothing, input)
+
+char :: Char -> Parser String Char
+char x = satisfy (x ==)
+
+word :: String -> Parser String String
+word "" = Parser $ \input -> (Just "", input)
+word (c:cs) = (:) <$> char c <*> word cs
+
+digit :: Parser String Int
+digit = digitToInt <$> satisfy isDigit
+
+digit19 :: Parser String Int
+digit19 = digitToInt <$> satisfy (\x -> isDigit x && x /= '0')
+
+hexDigit :: Parser String Int
+hexDigit = digitToInt <$> satisfy isHexDigit
+
+digitsToNumber :: Int -> [Int] -> Integer
+digitsToNumber base = foldl (\num d -> num * fromIntegral base + fromIntegral d) 0
+
+spaces :: Parser String String
+spaces = many $ satisfy isSpace
+
+eatSpaces :: Parser String a -> Parser String a
+eatSpaces p = spaces *> p <* spaces
+
+sepBy :: Parser i v -> Parser i s -> Parser i [v]
+sepBy v s = (:) <$> v <*> many (s *> v) <|> pure []
+
+jsonNull :: Parser String JsonValue
+jsonNull = word "null" $> JsonNull
+
+jsonBool :: Parser String JsonValue
+jsonBool = (word "true" $> JsonBool True) <|> (word "false" $> JsonBool False)
+
+jsonString :: Parser String JsonValue
+jsonString = JsonString <$> jsonStr
+
+jsonStr :: Parser String JsonStr
+jsonStr = JsonStr <$> (char '"' *> many jsonChar <* char '"')
+  where
+    jsonChar =  satisfy (`notElem` ['"', '\\'])
+            <|> word "\\\"" $> '"'
+            <|> word "\\\\" $> '\\'
+            <|> word "\\/"  $> '/'
+            <|> word "\\b"  $> '\b'
+            <|> word "\\f"  $> '\f'
+            <|> word "\\n"  $> '\n'
+            <|> word "\\r"  $> '\r'
+            <|> word "\\t"  $> '\t'
+            <|> chr . fromIntegral . digitsToNumber 16 <$> (word "\\u" *> replicateM 4 hexDigit)
+
+jsonNumber :: Parser String JsonValue
+jsonNumber = JsonNumber <$> (jsonIntFracExp <|> jsonIntExp <|> jsonIntFrac <|> jsonInt')
+  where
+    jsonIntExp = JsonNum <$> jsonInt <*> jsonExp
+
+    jsonIntFrac = makeIntFrac <$> jsonInt <*> jsonFrac
+    -- makeIntFrac significand mantissa
+    makeIntFrac s m = JsonNum (foldl (\num d -> num * 10 + fromIntegral d) s m)
+                              (fromIntegral . negate . length $ m)
+
+    jsonIntFracExp = (\(JsonNum s e) exp -> JsonNum s (e + exp)) <$> jsonIntFrac <*> jsonExp
+
+    jsonInt' = JsonNum <$> jsonInt <*> pure 0
+    jsonInt  = negate <$> (char '-' *> jsonUint) <|> jsonUint
+
+    jsonUint = (\d ds -> digitsToNumber 10 (d:ds)) <$> digit19 <*> some digit
+              <|> fromIntegral <$> digit
+
+    jsonFrac = char '.' *> some digit
+
+    jsonExp = (char 'e' <|> char 'E') *> optional (char '+') *> jsonInt
+
+jsonArray :: Parser String JsonValue
+jsonArray = JsonArray <$> (char '[' *> eatSpaces (json `sepBy` char ',') <* char ']')
+
+jsonObject :: Parser String JsonValue
+jsonObject = JsonObject <$> (char '{' *> pair `sepBy` char ',' <* char '}')
+  where
+    pair = (,) <$> eatSpaces jsonStr <* char ':' <*> json
+
+json :: Parser String JsonValue
+json = eatSpaces json'
+  where
+    json' =  jsonNull
+         <|> jsonBool
+         <|> jsonString
+         <|> jsonNumber
+         <|> jsonArray
+         <|> jsonObject
